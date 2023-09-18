5a61c66d2b96a48f10c0b048e6a92990bcfb8945
João Silva <138931914+joaov-rsilva@users.noreply.github.com>
2023-09-18 19:34:47 -0300

    Add files via upload

---
+++ Capítulo_9/Passagem_por_valor/Ex02.c
@@ -0,0 +1,36 @@
+/* 2-Faça uma função que receba um número inteiro de 1 a 12 e imprima em tela o mês 
+e a sua quantidade de dias de acordo com o número digitado pelo usuário. Exemplo: 
+Entrada = 4. Saída = abril. */
+
+#include <stdio.h>
+
+void meses_do_ano(int mes){
+    switch(mes){
+        case 1: printf("Janeiro (31 dias)\n"); break;
+        case 2: printf("Fevereiro (28/29 dias)\n"); break;
+        case 3: printf("Março (31 dias)\n"); break;
+        case 4: printf("Abril (30 dias)\n"); break;
+        case 5: printf("Maio (31 dias)\n"); break;
+        case 6: printf("Junho (30 dias)\n"); break;
+        case 7: printf("Julho (31 dias)\n"); break;
+        case 8: printf("Agosto (31 dias)\n"); break;
+        case 9: printf("Setembro (30 dias)\n"); break;
+        case 10: printf("Outubro (31 dias)\n"); break;
+        case 11: printf("Novembro (30 dias)\n"); break;
+        case 12: printf("Dezembro (31 dias)\n"); break;
+    }
+}
+
+int main(){
+
+    int op;
+
+    do{
+        printf("Digite um valor de 1 a 12: ");
+        scanf("%d", &op);
+    }while(op < 1 || op > 12);
+
+    meses_do_ano(op);
+
+    return 0;
+}
