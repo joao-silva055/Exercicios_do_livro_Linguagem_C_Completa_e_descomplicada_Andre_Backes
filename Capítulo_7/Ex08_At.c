86ac1643b5410c74128b7a53f9295a1bb41bb7fc
João Silva <joaovrsilva.ctt@gmail.com>
2023-11-27 15:04:18 -0300

    Update Ex08_At.c

--- Capítulo_7/Ex08_At.c
+++ Capítulo_7/Ex08_At.c
@@ -2,37 +2,42 @@
 informando se a segunda string está contida dentro da primeira. */
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#define MAX_STR 50
 
 int main(){
 
-    char str1[20], str2[20], aux[20];
-    int i, j;
+    char str1[MAX_STR], str2[MAX_STR];
 
-    printf("Digite a primeira string: ");
-    fgets(str1, 20, stdin);
-    printf("Digite a segunda string: ");
-    fgets(str2, 20, stdin);
+    printf("Digite a 1ª string: ");
+    fgets(str1, MAX_STR, stdin);
+    str1[strcspn(str1, "\n")] = '\0';
+    
+    printf("Digite a 2ª string: ");
+    fgets(str2, MAX_STR, stdin);
+    str2[strcspn(str2, "\n")] = '\0';
 
-    str1[strlen(str1)-1] = '\0';
-    str2[strlen(str2)-1] = '\0';
-
-    for( i = 0, j = 0; str1[i] != '\0'; i++){
-        while(str1[i] != ' ' && str1[i] != '\0'){
-            aux[j] = str1[i];
-            i++;
-            j++;
-        }
-        aux[j] = '\0';
-        if(strcmp(aux, str2) == 0){
-            printf("String 2 está dentro da strin 1.\n");
-            return 0;
-        }else{
-            j = 0;
+    int ctt = 0, flag = 0;
+    for(int i = 0; str1[i]!='\0'; i++){
+        if(str2[0] == str1[i] && flag == 0){
+            for(int j = 0; j < strlen(str2); j++){
+                if((str2[j] == str1[j+i]) && (flag == 0)){
+                    ctt++;
+                }else{
+                    ctt = 0;
+                }
+                if(ctt == strlen(str2)){
+                    flag++;
+                }
+            }
         }
     }
-    printf("A string 2 não está dentro da string 1.\n");
+
+    if(ctt == strlen(str2)){
+        printf("String 2 aparece dentro de string 1.\n");
+    }else{
+        printf("String 2 não aparece dentro de string 1.\n");
+    }
 
     return 0;
 }
