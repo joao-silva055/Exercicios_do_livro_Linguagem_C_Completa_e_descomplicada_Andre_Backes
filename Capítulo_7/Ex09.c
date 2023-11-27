460b4d52096a63978d0a67079279093475a362b7
João Silva <joaovrsilva.ctt@gmail.com>
2023-11-27 20:28:32 -0300

    Update Ex09_At.c

--- Capítulo_7/Ex09.c
+++ Capítulo_7/Ex09.c
@@ -2,37 +2,35 @@
 informando quantas vezes a segunda string lida está contida dentro da primeira. */
 
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#define MAX_STR 100
 
 int main(){
 
-    char str1[20], str2[20], aux[20];
-    int i, j, contagem = 0;
+    char str1[MAX_STR], str2[MAX_STR], aux[MAX_STR];
 
-    printf("Digite a primeira string: ");
-    fgets(str1, 20, stdin);
-    printf("Digite a segunda string: ");
-    fgets(str2, 20, stdin);
+    printf("Digite a 1ª string: ");
+    fgets(str1, MAX_STR, stdin);
+    printf("Digite a 2ª string: ");
+    fgets(str2, MAX_STR, stdin);
+    
+    //Remoção do caractere '\n' (New Line) lido pelo fgets()
+    str1[strcspn(str1, "\n")] = '\0';
+    str2[strcspn(str2, "\n")] = '\0';
 
-    str1[strlen(str1)-1] = '\0';
-    str2[strlen(str2)-1] = '\0';
-
-    for( i = 0, j = 0; str1[i] != '\0'; i++){
-        while(str1[i] != ' ' && str1[i] != '\0'){
-            aux[j] = str1[i];
-            i++;
-            j++;
-        }
-        aux[j] = '\0';
-        if(strcmp(aux, str2) == 0){
-            contagem++;
-            j = 0;
-        }else{
-            j = 0;
+    int ctt = 0;
+    for(int i = 0; str1[i] != '\0'; i++){
+        if(str2[0] == str1[i]){
+            for(int j = 0; j < strlen(str2); j++){
+                aux[j] = str1[j+i];
+            }
+            if(!strcmp(aux, str2)){
+                ctt++;
+            }
         }
     }
-    printf("A string 2 aparece %d vezes dentro da string 1.\n", contagem);
+
+    printf("A string 2 está presente %d vezes dentro da string 1.\n", ctt);
 
     return 0;
 }
