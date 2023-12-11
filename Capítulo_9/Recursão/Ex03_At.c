19c899dbee480c3bc8340395a20c1da4fe095c4d
João Silva <joaovrsilva.ctt@gmail.com>
2023-12-11 13:13:06 -0300

    Update Ex03_At.c

--- Capítulo_9/Recursão/Ex03_At.c
+++ Capítulo_9/Recursão/Ex03_At.c
@@ -4,13 +4,13 @@ naturais de 0 até N em ordem crescente */
 #include <stdio.h>
 #include <stdlib.h>
 
-int Print_in_ascending_order(int n){
-
+void Print_in_ascending_order(int n){
+    //Caso base
     if(n == 0){
-        //Caso base
-        return printf("%d\n", n);
+        printf("%d\n", n);
     }else{
-        //Interrompe a primeira chamada chamada; Inicio do loop
+        //Interrompe a primeira chamada chamada
+        //Inicio do loop
         Print_in_ascending_order(n - 1);
         //Só será executado depois de descarregar toda memória voltando a primeira chamada
         printf("%d\n", n);
